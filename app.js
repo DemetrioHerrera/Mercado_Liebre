f6bad23878569c8a31e064635afb25cac575e5a9
Luis Demetrio Herrera Cobos <luisherreracobos@gmail.com>
2021-06-30 20:42:31 -0500

    Se configura el puerto del servidor  para usar variables de entorno

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get("/registro",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.sendFile(__dirname + "/views/login.html");
 });
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Servidor Levantado");
 })
